a6d6dc91e078588f7caf475e2e00b1e62540a097
zaftie <zaftreal@gmail.com>
2024-01-24 16:25:41 -0800

    basics

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"; 
+
+function Footer(){
+    const currYear = new Date().getFullYear();
+    return <footer><p> Copyright {currYear}</p></footer>;
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App /> , document.getElementById("root"));
 //1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
